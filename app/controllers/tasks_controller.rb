acbf9eff37815f32d3c57717612716eeeed443e8
Victor Henao <victorhenao@gmail.com>
2016-09-27 20:02:36 -0500

    Creatre controller for tasks, and the base index interface

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,19 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+    @categories = Category.all
+    @projects = Project.all
+  end
+
+  def create
+    @task = Task.new(post_params)
+      if task.save
+          flash[:notice] = "Task created!"
+      end
+  end
+
+  private 
+    def post_params
+      params.require(:task).permit(:name, :description, :category_id, :project_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: 'sessions#new'
   resources :sessions, only: :index
-  get "/auth/:provider/callback" => 'sessions#create'
   resource :dashboard, only: :show
+  
+  get "/auth/:provider/callback" => 'sessions#create'
+  get 'tasks/index'
+  get 'tasks/create'
+  
+  
 end
